38798d41a7c5aa02a45ba87cf469eae3845f4597
follower <follower@rancidbacon.com>
2019-05-15 04:07:57 +1200

    Move `setMessage()` into main player script for now to enable reuse.

--- js/gifuct-minimal-player-with-audio-gif-support.js
+++ js/gifuct-minimal-player-with-audio-gif-support.js
@@ -67,6 +67,24 @@ function wrapWithLink(gifUrl) {
 
 }
 
+
+function setMessage(target_ctx, msg_fill_style, font_size_line_1, font_size_line_2, msg_line_1, msg_line_2) {
+
+        // TODO: Do this better... :)
+        target_ctx.textAlign = "center";
+        target_ctx.textBaseline = "middle";
+
+        target_ctx.fillStyle = msg_fill_style;
+
+        target_ctx.font = font_size_line_1 + "px sans-serif";
+        target_ctx.fillText(msg_line_1, target_ctx.canvas.width/2, ((target_ctx.canvas.height-font_size_line_1)/2)-(font_size_line_1*0.625));
+
+        target_ctx.font = font_size_line_2 + "px sans-serif";
+        target_ctx.fillText(msg_line_2, target_ctx.canvas.width/2, ((target_ctx.canvas.height-font_size_line_2)/2)+(font_size_line_2*0.625));
+
+}
+
+
 function renderGIF(frames){
         loadedFrames = frames;
         frameIndex = 0;

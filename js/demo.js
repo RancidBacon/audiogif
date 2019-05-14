e31415e640345d621c4f64efe415e37df07a7c5f
follower <follower@rancidbacon.com>
2019-05-15 04:40:21 +1200

    Move `setGIF()` into player script since probably now not optional.

--- js/demo.js
+++ js/demo.js
@@ -1,17 +1,2 @@
 
-function setGIF(url) {
-
-  // Everyone loves a "loading..." message...
-  // TODO: Do this better... :)
-  const placeholder_width = 400; // px
-  const font_size = 14; // px
-
-  c.width = placeholder_width;
-  c.height = placeholder_width;
-
-  setMessage(ctx, "#eb99a1", font_size, font_size, "Audio GIF", "loading");
-
-  // Actually start loading of the file...
-  loadGIF(url);
-
-}
+// There *used* to be something here...
--- js/gifuct-minimal-player-with-audio-gif-support.js
+++ js/gifuct-minimal-player-with-audio-gif-support.js
@@ -85,6 +85,24 @@ function setMessage(target_ctx, msg_fill_style, font_size_line_1, font_size_line
 }
 
 
+function setGIF(url) {
+
+        // Everyone loves a "loading..." message...
+        // TODO: Do this better... :)
+        const placeholder_width = 400; // px
+        const font_size = 14; // px
+
+        c.width = placeholder_width;
+        c.height = placeholder_width;
+
+        setMessage(ctx, "#eb99a1", font_size, font_size, "Audio GIF", "loading");
+
+        // Actually start loading of the file...
+        loadGIF(url);
+
+}
+
+
 function renderWhenAudioReady(gifUrl) {
 
         // Due to chang(ing|ed) WebAudio auto-play policies we need to detect when
